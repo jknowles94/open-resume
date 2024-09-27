a199fcfdb33bebcca213a49a4e845795fb7651c7
Jake Knowles <jakeknowles@C02FW649MD6M.home>
2024-09-27 15:02:41 +0100

    add profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import Profile from './components/Profile';
 
 function App() {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-4">
-        <div className="bg-red-300 col-span-1">
-          <h2>Profile</h2>
+      <div className="grid grid-cols-1 lg:grid-cols-4">
+        <div className="bg-neutral-800 col-span-1 h-screen p-4 py-8 flex-col flex items-center">
+          <Profile />
           <h2>BIO</h2>
           <h2>CONTACT</h2>
           <h2>Skills</h2>
         </div>
         <div className="col-span-3">
-          <h1 className="text-3xl font-bold underline bg-red-300 p-8">
+          <h1 className="text-3xl font-bold underline bg-emerald-700 p-8">
             Intro
           </h1>
           <h1 className="text-3xl font-bold underline">
---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import profile from '/id.jpg'
+
+function Profile() {
+
+  return (
+    <img src={profile} alt="Jake Knowles" className='lg:w-[200px] lg:h-[200px] rounded-full border-emerald-700 border-4'/>
+  )
+}
+
+export default Profile
