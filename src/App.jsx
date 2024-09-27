fdae0a419867a297d8a3d6e36efc7a8030678ef3
Jake Knowles <jakeknowles@C02FW649MD6M.home>
2024-09-27 12:05:50 +0100

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,31 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-4">
+        <div className="bg-red-300 col-span-1">
+          <h2>Profile</h2>
+          <h2>BIO</h2>
+          <h2>CONTACT</h2>
+          <h2>Skills</h2>
+        </div>
+        <div className="col-span-3">
+          <h1 className="text-3xl font-bold underline bg-red-300 p-8">
+            Intro
+          </h1>
+          <h1 className="text-3xl font-bold underline">
+            Experiences
+          </h1>
+          <h1 className="text-3xl font-bold underline">
+            Personal Projects
+          </h1>
+          <h1 className="text-3xl font-bold underline">
+            Education
+          </h1>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
